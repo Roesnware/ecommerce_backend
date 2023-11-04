af483b6b00b32f27a5185c285ccfc557c60e8215
Roesn <Bubwub04@icloud.com>
2023-11-04 16:57:19 -0400

    tag model

--- models/Tag.js
+++ models/Tag.js
@@ -11,6 +11,36 @@ class Tag extends Model {}
 Tag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true
+      }
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: "category",
+      referencesKey: "id"
+    }
   },
   {
     sequelize,
