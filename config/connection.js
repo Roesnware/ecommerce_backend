3c83ddadd39c14d27a3e8d3f171f6312794c8fed
Roesn <Bubwub04@icloud.com>
2023-11-04 14:34:33 -0400

    connection.js commented

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,19 @@
+// import module dotenv
 require('dotenv').config();
 
+// import module sequlize
 const Sequelize = require('sequelize');
 
+// initializzw connection 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: 'localhost',
-      dialect: 'mysql',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
+    host: 'localhost',
+    dialect: 'mysql',
+    dialectOptions: {
+      decimalNumbers: true,
+    },
+  });
 
+// export module 
 module.exports = sequelize;
