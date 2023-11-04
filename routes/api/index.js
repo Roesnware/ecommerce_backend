1f20b60b41f0557cd26cdb118d0c5ef3b0b299f8
Roesn <Bubwub04@icloud.com>
2023-11-04 14:38:06 -0400

    comments

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,13 @@
+// import modules 
 const router = require('express').Router();
 const categoryRoutes = require('./category-routes');
 const productRoutes = require('./product-routes');
 const tagRoutes = require('./tag-routes');
 
+// specific routes 
 router.use('/categories', categoryRoutes);
 router.use('/products', productRoutes);
 router.use('/tags', tagRoutes);
 
+// export module
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
+// import modules 
 const router = require('express').Router();
 const apiRoutes = require('./api');
 
+// use api routes
 router.use('/api', apiRoutes);
 
+// bad req 
 router.use((req, res) => {
   res.send("<h1>Wrong Route!</h1>")
 });
 
+// export module 
 module.exports = router;
